2a193310db8a2b6f0c4ae551f1417d9d861fc88c
Vandan Patel <vandan@Vandans-MacBook-Air.local>
2026-04-21 23:13:22 -0400

    Completed menu.php dropdown list

---
+++ menu.php
@@ -0,0 +1,25 @@
+<?php
+require ('db_connect.php');
+
+try {
+    $query = new MongoDB\Driver\Query([], []);
+    
+    $cursor = $manager->executeQuery("$dbName.bids", $query);
+
+    echo '
+    <form action="bidSheet.php" method="POST"> Choose an item to bid on: <select name="item_name"></select>';
+    
+    foreach ($cursor as $document) {
+        echo '<option value="' . $document->item . '">' . $document->item . '</option>';
+    }
+    
+    echo ' 
+        </select>
+        <input type="submit" value="Submit">
+    </form>'
+  ;
+
+} catch (Exception $e) {
+    echo "Could not connect:\n";
+}
+?>
