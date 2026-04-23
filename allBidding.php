7cccdfcc1a2a8b512ac881e80222dec955e64736
nshidle <nshidle@icloud.com>
2026-04-23 19:44:25 -0400

    Fetch and display bids from MongoDB
    Implemented MongoDB query to fetch bids and display them in a dropdown table format with date and price.

--- allBidding.php
+++ allBidding.php
@@ -1,11 +1,25 @@
 <?php
 require ('db_connect.php')
-db_connect.collection.find()
-for i in item:
-  for entry in history:
-    make a dropdown table
-
-
-
-
+try {
+    $queryMongo = new MongoDB\Driver\Query([], []);
+    
+    $result = $manager->executeQuery("$dbName.bids", $queryMongo);
+    foreach ($result as $document) {
+      echo '<p>' . $document->item . '</p>';
+      echo '<table>';
+      echo '<tr>
+          <th>Date</th>
+          <th>Price</th>
+        </tr>';
+      foreach ($document->history as $entry){
+         echo '<tr>';
+         echo "<td>" . $entry->when . "</td>";
+         echo "<td>" . $entry->price . "</td>";
+         echo '</tr>';
+        
+          }
+        echo '</table>';
+    }
+} catch (Exception $e) {
+    echo "Could not connect\n";
 ?>
