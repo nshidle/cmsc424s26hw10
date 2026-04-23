014599fb51dab712fc90bf13c7816c53f233e739
nshidle <nshidle@icloud.com>
2026-04-23 19:57:31 -0400

    Implement data reset for items in MongoDB
    Reset data for items in the database with new history entries.

--- reset.php
+++ reset.php
@@ -1,9 +1,10 @@
 <?php
 require ('db_connect.php')
 
-
-
-
-
-
+$bulk = new MongoDB\Driver\Bulkwrite();
+$bulk->update(['item' => 'table'] , ['$set' => ['history' => [['when' => '2026-03-20' , 'price' => 1]]]]);
+$bulk->update(['item' => 'phone'] , ['$set' => ['history' => [['when' => '2026-03-20' , 'price' => 1]]]]);
+$bulk->update(['item' => 'laptop'] , ['$set' => ['history' => [['when' => '2026-03-20' , 'price' => 1]]]]);
+$manager->executeBulkWrite("$dbName.bids", $bulk);
+echo 'Data has been reset';
 ?>
